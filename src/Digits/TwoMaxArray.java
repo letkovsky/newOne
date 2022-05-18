e8c43c505db3d80646dbdeb0c3734cc64ce84118
letkovsky <summerkovski@gmail.com>
2022-05-18 16:55:51 +0300

    commit 18/05 some Modification

--- src/Digits/TwoMaxArray.java
+++ src/Digits/TwoMaxArray.java
@@ -1,4 +1,21 @@
 package Digits;
 
 public class TwoMaxArray {
+    public static int findSecondMax(int[] arr) {
+        int max = arr[0]; // max
+        int temp = arr[0]; // second max
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                temp = max;
+                max = arr[i];
+            }
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new int[]{3,4,5,8,4,56,34,23,77};
+        System.out.println(findSecondMax(ints));
+    }
+
 }
